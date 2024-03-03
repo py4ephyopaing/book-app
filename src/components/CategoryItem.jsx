c2e7fd146806312e0925ad1670978f31693023cc
Maung <maung.alive@gmail.com>
2024-03-03 20:31:44 +0630

    add more style

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function CategoryItem({ name, image }) {
   return (
-    <div className="relative w-full h-[200px] bg-slate-800 rounded-xl group">
-        <p className="py-4 pl-5 text-4xl text-white font-medium font-mukta">{name}</p>
-        <img src={image} alt={name} className="group-hover:-translate-y-[30%] duration-200 absolute -translate-y-[40%] top-0 right-0 h-full" />
+    <div className="relative w-full h-[200px] bg-slate-200 dark:bg-slate-800 rounded-xl group cursor-pointer transition-all duration-200 hover:scale-95 hover:bg-slate-300 dark:hover:bg-slate-900 hover:border-x-4 dark:border-white border-black hover:px-4">
+        <p className="py-4 pl-5 text-4xl group-hover:text-blue-900 text-black dark:group-hover:text-blue-100 dark:text-white font-medium font-mukta">{name}</p>
+        <img src={image} alt={name} className="group-hover:-translate-y-[70%] duration-200 absolute top-1/2 -translate-y-1/2 right-3 h-[90%]" />
     </div>
   )
 }
