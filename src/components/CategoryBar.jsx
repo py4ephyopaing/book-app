711b3390c6e24961b49d33c96a3cbf6f4052445c
Maung <maung.alive@gmail.com>
2024-03-03 15:56:44 +0630

    Category Bar component dark mode add

--- src/components/CategoryBar.jsx
+++ src/components/CategoryBar.jsx
@@ -17,7 +17,7 @@ function CategoryBar({ items }) {
 
 	return (
 		<div className="mt-4 mb-2 flex items-center gap-2 flex-wrap">
-			<span className="text-md font-medium text-white py-1 pr-3">Category Filter: </span>
+			<span className="text-md font-medium text-gray-900 dark:text-gray-100 py-1 pr-3">Category Filter: </span>
 			{
 				items.map(item => <span active={false} onClick={handleCategoryClick} className="bg-blue-200 dark:bg-indigo-950 text-black dark:text-white px-3 py-1 rounded-lg font-medium cursor-pointer">{item}</span> )
 			}
