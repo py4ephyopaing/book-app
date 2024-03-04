40a16ef6954b31d63e44b473a6ef26a87ed754b2
Maung <maung.alive@gmail.com>
2024-03-04 11:12:00 +0630

    Add star rating

--- src/components/BookItem.jsx
+++ src/components/BookItem.jsx
@@ -3,17 +3,22 @@ import { FaStar, FaStarHalf } from 'react-icons/fa'
 
 export default function BookItem({img, title, description, rating}) {
     const calculateStars = (rating) => {
-        return <FaStar className='text-yellow-400' /> ** rating.toFixed()
+        let stars = [];
+        
     }
 
     return (
-        <div className="rounded-xl w-full h-auto px-5 md:px-10 py-2 flex items-start gap-2">
-            <img src={img} alt={title} className="w-[100px] h-[150px] object-cover rounded-md" />
+        <div className="rounded-xl w-full overflow-hidden h-[160px] mb-2 px-5 md:px-10 py-2 flex items-start gap-2 group">
+            <img src={img} alt={title} className="duration-200 w-[100px] h-[150px] object-cover rounded-md" />
             <div className="">
-                <p className="text-2xl py-2 font-medium text-black dark:text-white font-mukta">{title}</p>
+                <p className="text-2xl duration-200 group-hover:text-3xl group-hover:text-blue-600 py-2 font-medium text-black dark:text-white font-mukta">{title}</p>
                 <p className="text-md text-gray-600 dark:text-gray-400 font-josefins">{description}</p>
                 <div className="flex gap-x-2 mt-2">
-                    {calculateStars(rating)}
+                    {
+                        Array.apply(null, { length: rating.toFixed() }).map((e, i) => (
+                            <FaStar className='text-yellow-400' />
+                        ))                          
+                    }
                     <span className="text-gray-700 dark:text-gray-300 -mt-1 pl-3">({rating})</span>
                 </div>
             </div>
