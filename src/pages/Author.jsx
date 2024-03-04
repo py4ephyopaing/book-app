4e637adeabf6c627808665e4f0a1dc50f9061f41
Maung <maung.alive@gmail.com>
2024-03-04 20:29:52 +0630

    Author page add

---
+++ src/pages/Author.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import AuthorCard from '../components/AuthorCard'
+
+export default function Author() {
+    return (
+        <div className="min-h-screen w-full bg-gray-100 dark:bg-slate-600 h-auto px-3 lg:px-8 py-2 lg:py-4">
+            <AuthorCard />
+        </div>
+    )
+}
