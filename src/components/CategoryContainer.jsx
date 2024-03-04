876aa074927fad061f6d7d4adbeeed866c139d82
Maung <maung.alive@gmail.com>
2024-03-04 13:49:41 +0630

    More responsive

--- src/components/CategoryContainer.jsx
+++ src/components/CategoryContainer.jsx
@@ -3,7 +3,7 @@ import CategoryItem from './CategoryItem'
 
 function CategoryContainer({ categories }) {
     return (
-        <div className="grid md:grid-cols-2 gap-y-2 gap-x-3">
+        <div className="grid lg:grid-cols-2 gap-y-2 gap-x-3">
             {categories.map((i, index) => <CategoryItem {...i} key={index} />)}
         </div>
     )
