c88a7c0d27272bb544b0a2cd91de0f4dc2bd2002
Maung <maung.alive@gmail.com>
2024-03-04 13:49:15 +0630

    Dark theme finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import SideBar from './components/SideBar'
 import ASide from './components/ASide'
-import Home from './pages/Home'
 import classNames from 'classnames';
 import Router from './Router';
+import { ThemeContext } from './contexts/ThemeContext';
 
 export default function App() {
+  const dark = localStorage.theme === "dark"
+  let [ showAside, setShowAside ] = useState(false)
+  let [ darkTheme, setDarkTheme ] = useState(dark)
 
-  let [ showAside, setShowAside ] = useState(false);
-  let [ darkTheme, setDarkTheme ] = useState(false);
+  useEffect(() => {
+    localStorage.theme = darkTheme ? "dark" : "light"
+  }, [darkTheme])
 
   return (
-    <div className={classNames({
-      "w-full bg-slate-600": true,
-      "dark": darkTheme
-    })}>
+    <div className={`${darkTheme ? 'dark' : 'light'} w-full bg-slate-600`}>
       <SideBar showAside={showAside} setShowAside={setShowAside} />
 
       <div className="lg:pl-[17rem] md:pr-[22rem]">
-        <Router />
+        <ThemeContext.Provider value={{ darkTheme, setDarkTheme }}>
+          <Router />
+        </ThemeContext.Provider>
       </div>
 
       <ASide setShowAside={setShowAside} showAside={showAside} />
--- src/components/ThemeSwitch.jsx
+++ src/components/ThemeSwitch.jsx
@@ -1,31 +1,24 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { ThemeContext } from '../contexts/ThemeContext';
 
-function ThemeSwitch() {
-	const [darkMode, setDarkMode] = useState(false);
-	const toggleDarkMode = () => {
-		setDarkMode(prev =>!prev);
-	}
-
-	useEffect(() => {
-		const root = document.body
-		root.classList.remove('light', 'dark')
-		root.classList.add(darkMode ? 'dark' : 'light');
-	}, [darkMode])
+function ThemeSwitch() {	
+	const { darkTheme, setDarkTheme} = useContext(ThemeContext)
 
 	return (
-		<div class="inline-flex items-center">
-			<div class="relative inline-block w-8 h-4 rounded-full cursor-pointer">
-				<input onClick={toggleDarkMode} id="switch" type="checkbox"
-					class="absolute w-8 h-4 transition-colors duration-300 rounded-full appearance-none cursor-pointer peer bg-blue-gray-100 checked:bg-gray-900 peer-checked:border-gray-900 peer-checked:before:bg-gray-900"
-				 />
+			<div className="inline-flex items-center">
+				<div className="relative inline-block w-8 h-4 rounded-full cursor-pointer">
+					<input onChange={() => setDarkTheme(!darkTheme)} id="switch" type="checkbox"
+						className="absolute w-8 h-4 transition-colors duration-300 rounded-full appearance-none cursor-pointer peer bg-blue-gray-100 checked:bg-gray-900 peer-checked:border-gray-900 peer-checked:before:bg-gray-900"
+						defaultChecked={darkTheme}
+					/>
 
-				<label htmlFor="switch"
-				class="before:content[''] absolute top-2/4 -left-1 h-5 w-5 -translate-y-2/4 cursor-pointer rounded-full border border-blue-gray-100 bg-white shadow-md transition-all duration-300 before:absolute before:top-2/4 before:left-2/4 before:block before:h-10 before:w-10 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity hover:before:opacity-10 peer-checked:translate-x-full peer-checked:border-gray-900 peer-checked:before:bg-gray-900">
-					<div class="inline-block p-5 rounded-full top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4"
-					data-ripple-dark="true"></div>
-				</label>
+					<label htmlFor="switch"
+					className="before:content[''] absolute top-2/4 -left-1 h-5 w-5 -translate-y-2/4 cursor-pointer rounded-full border border-blue-gray-100 bg-white shadow-md transition-all duration-300 before:absolute before:top-2/4 before:left-2/4 before:block before:h-10 before:w-10 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity hover:before:opacity-10 peer-checked:translate-x-full peer-checked:border-gray-900 peer-checked:before:bg-gray-900">
+						<div className="inline-block p-5 rounded-full top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4"
+						data-ripple-dark="true"></div>
+					</label>
+				</div>
 			</div>
-		</div>
 	)
 }
 
