42a860d5d2c28056cf89c906c0937f119ef4e699
Maung <maung.alive@gmail.com>
2024-03-06 23:57:08 +0630

    rename

--- src/components/AuthorCard.jsx
+++ src/components/AuthorCard.jsx
@@ -12,7 +12,7 @@ function AuthorCard() {
     <div className='w-full px-4 lg:px-10 py-10 shadow-md md:flex gap-x-4'>
         <img src="/tolkien.jpg" alt="Tolkien" className='w-[150px] h-[150px] mx-auto mb-4 md:mb-0 lg:w-[180px] lg:h-[180px] object-cover rounded-md' />
         <div className='text-center md:text-left'>
-            <p className="text-2xl font-medium font-mukta dark:text-white flex gap-x-4 items-center justify-center md:justify-normal">Christopher Tolkien <FaWifi onClick={handleFollow} title='Follow' className='-rotate-45 active:scale-125 hover:text-fuchsia-600' /></p>
+            <p className="text-2xl font-medium font-mukta dark:text-white flex gap-x-4 items-center justify-center md:justify-normal">John Ronald Reuel Tolkien <FaWifi onClick={handleFollow} title='Follow' className='-rotate-45 active:scale-125 hover:text-fuchsia-600' /></p>
 
             <p className="text-base md:text-lg font-nunito font-thin pt-3 dark:text-gray-100">
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis aliquid sapiente eaque nulla officia dolorum rem praesentium cumque deleniti totam debitis.
