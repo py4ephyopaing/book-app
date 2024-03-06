9ecb34367673cc7e245447a416b9b44d57185c58
Maung <maung.alive@gmail.com>
2024-03-06 14:23:35 +0630

    Just update style

--- src/components/AuthorCard.jsx
+++ src/components/AuthorCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaPeopleArrows, FaWifi } from 'react-icons/fa'
+import { FaBook, FaStar, FaWifi } from 'react-icons/fa'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -14,13 +14,13 @@ function AuthorCard() {
         <div className='text-center md:text-left'>
             <p className="text-2xl font-medium font-mukta dark:text-white flex gap-x-4 items-center justify-center md:justify-normal">Christopher Tolkien <FaWifi onClick={handleFollow} title='Follow' className='-rotate-45 active:scale-125 hover:text-fuchsia-600' /></p>
 
-            <p className="text-xl font-nunito font-thin pt-3 dark:text-gray-100">
+            <p className="text-base md:text-lg font-nunito font-thin pt-3 dark:text-gray-100">
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis aliquid sapiente eaque nulla officia dolorum rem praesentium cumque deleniti totam debitis.
             </p>
 
             <p className='justify-center md:justify-normal flex gap-x-3 pt-2'>
                 <span className="dark:text-gray-200 text-gray-800 flex items-center gap-x-2"><FaWifi className='-rotate-45' /> Followers &middot; 13M</span> &nbsp;&nbsp;
-                <span className="dark:text-gray-200 text-gray-800 flex items-center gap-x-2"><FaPeopleArrows /> Following &middot; 0</span>
+                <span className="dark:text-gray-200 text-gray-800 flex items-center gap-x-2"><FaStar /> Rating &middot; 4.2</span>
             </p>
         </div>
         <ToastContainer />
